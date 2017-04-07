080a590531ce9ff8b98f7919eb5d87d0f0d5d090
Antonio Pierro <antonio.pierro@gmail.com>
2017-04-07 10:06:53 +0200

    Added html test

--- test/StupidTest.php
+++ test/StupidTest.php
@@ -1,12 +1,23 @@
 <?php
 
-namespace phpUnitTutorial\Test;
-
 class StupidTest extends \PHPUnit_Framework_TestCase
 {
     public function testTrueIsTrue()
     {
         $foo = true;
         $this->assertTrue($foo);
+
+
+        $matcher = array('id' => 'my_id');
+        $sOutput = '<p id="my_id">';
+
+    }
+
+    public function testHTML()
+    {
+
+        $matcher = array('id' => 'my_id');
+        $htmlOutput = '<p id="my_id">ciao</p>';
+        $this->assertTag($matcher, $htmlOutput);
     }
 }
