0d429e11915f0e0c067cdc34570130a13110f128
Antonio Pierro <antonio.pierro@gmail.com>
2017-04-14 16:15:01 +0200

    Added RegExp test

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
     <title>test</title>
 </head>
 <body>
+    <h1 class="title">Test title text</h1>
     <div class="foo">Test class text</div>
+    <ul>
+        <li></li>
+        <li></li>
+    </ul>
+    <div class="bar">Antonio Pierro</div>
 </body>
 </html>
